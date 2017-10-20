c0d3063f509e2bfd310c0df0e267b5145d64951e
nnnam0048 <vagrant@homestead>
2017-10-20 04:54:20 +0000

    init model and migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Field;
+use App\Level;
+use App\Topic;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Topic::truncate();
+        Field::truncate();
+
+        factory(User::class, 30)->create();
+        factory(Field::class, 30)->create();
+        factory(Level::class, 30)->create();
+        factory(Topic::class, 30)->create();
+
     }
 }
