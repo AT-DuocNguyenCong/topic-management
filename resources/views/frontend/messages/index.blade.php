b4dc1182fbe64edefd9de0d38fc65376eb7df5d0
Duoc Nguyen C <satthukt1232@gmail.com>
2017-11-09 14:51:34 +0700

    fix messages user

--- resources/views/frontend/messages/index.blade.php
+++ resources/views/frontend/messages/index.blade.php
@@ -1,28 +1,40 @@
 @extends('frontend.layouts..main')
 @section('title', __('Messages | '. Auth::user()->full_name))
 @section('content')
-  <div id="fh5co-media-section">
-    <div class="container">
-      @include('flash::message')
-      @include('backend.layouts.partials.modal')
-      @foreach($messages as $message)
-        <div class="alert cls-alert-info" style="font-size: 18px">
-          <strong>From: {{ __($message->reciever->full_name) }}</strong>
-          <p><strong>Content:</strong></p>
-          {!! $message->content !!}
-          
-          <form method="POST" action="{{ route('handlerequest.destroy', $message->id) }}" class="inline">
-            {!! csrf_field() !!}
-            {{ method_field('DELETE') }}
-            <button type="submit" 
-              class="btn btn-primary pull-right btn-delete-item"
-              data-title="{{ __('Confirm deletion!') }}"
-              data-confirm="{{ __('Are you sure you want to delete?') }}">Remove
-            </button>
+  <div class=" container content-wrapper">
+    <div class="content-wrapper">
+      <section class="content">
+        @include('flash::message')
+        @include('backend.layouts.partials.modal')
+        <div class="row">
+          @foreach($messages as $message)
+            <div class="alert cls-alert-info" style="font-size: 18px">
+              <strong>Sender: {{ __($message->reciever->full_name) }}</strong>
+              <p><strong>Content:</strong></p>
+              <div class="row">
+                <div style="margin-left: 1%">
+                  <textarea readonly class="col-md-9" style="resize: none">{!! $message->content !!}</textarea>
+                </div>
+                <div class="col-md-2 pull-right" style="margin-top: 10px">
+                  <form method="POST" action="{{ route('handlerequest.destroy', $message->id) }}" class="inline">
+                  {!! csrf_field() !!}
+                  {{ method_field('DELETE') }}
+                  <button type="submit" 
+                    class="btn btn-primary pull-right btn-delete-item"
+                    data-title="{{ __('Confirm deletion!') }}"
+                    data-confirm="{{ __('Are you sure you want to delete?') }}">Remove
+                  </button>
+                  </form>
+                </div>
+                
+              </div>
+            </div>
+          @endforeach
+          <div class="pull-right cls-mr-13">
+            {!! $messages->render() !!}
+          </div>
         </div>
-      @endforeach
-      <div class="pull-right cls-mr-13">
-        {!! $messages->render() !!}
-      </div>
+      </section>
+    </div>
   </div>
 @endsection
